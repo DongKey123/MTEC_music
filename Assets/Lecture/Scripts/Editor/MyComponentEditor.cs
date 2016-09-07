94de190393b3926b7a6a3f4e2cea6cd82278db04
DongKey123 <kdh2183@gmail.com>
2016-09-07 09:41:31 +0900

    Day - 1 수업
    -유니티 프로젝트 생성
    -MyComponent 연습

---
+++ Assets/Lecture/Scripts/Editor/MyComponentEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomEditor(typeof(MyComponent))]
+public class MyComponentEditor : Editor
+{
+    void OnEnable() //클릭하는순간
+    {
+    }
+
+    public override void OnInspectorGUI() //보여지는동안
+    {
+        Debug.Log("Editor");
+        ////base : 상위 객체
+        //base.OnInspectorGUI();
+
+        MyComponent myComponent = (MyComponent)target;
+        myComponent.intVarialble = EditorGUILayout.IntField("Int Variable", myComponent.intVarialble);
+        myComponent.floatVariable = EditorGUILayout.Slider("Float Variable", myComponent.floatVariable, 0f, 1f);
+
+        myComponent.intVarialble = EditorGUILayout.IntField("Int Variable", myComponent.IntVar);
+        //myComponemt.intVarialble = EditorGUILayout.IntField("Int Variable", myComponemt.IntVar);
+
+        if (GUILayout.Button("Do something") == true)
+        {
+            myComponent.DoSomethig();
+        }
+
+    }
+
+}
---
+++ Assets/Lecture/Scripts/MyComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour {
+
+    public int intVarialble;
+    public float floatVariable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DoSomethig()
+    {
+        intVarialble++;
+    }
+
+    public int IntVar
+    {
+        get
+        {
+            return intVarialble;
+        }
+        set
+        {
+            intVarialble = value;
+        }
+    }
+}
